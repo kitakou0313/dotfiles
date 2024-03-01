1694b7729b2ffd3703c5153a0a7100e779a20bec
kitakou0313 <kitakou0313@gmail.com>
2024-03-01 23:47:30 +0900

    fix:init file

--- installer.sh
+++ installer.sh
@@ -4,9 +4,9 @@ set -e
 # インストール用のスクリプト
 
 # Clone repository
-mkdir tmp-for-init
-cd tmp-for-init
-git pull https://github.com/kitakou0313/dotfiles
+mkdir tmp-for-dotfiles
+cd tmp-for-dotfiles
+git clone https://github.com/kitakou0313/dotfiles
 
 
 # Copy each files
@@ -15,3 +15,5 @@ cat .bashrc >> ~/.bashrc
 cat .bash_profile >> ~/.bash_profile
 cat .gitconfig >> ~/.gitconfig
 cat .tmux.conf >> ~/.tmux.conf
+
+rm -rf tmp-for-dotfiles
